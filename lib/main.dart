06fa7042c1b86c7825a68df0f0cb446ad4ca8ddc
satyam Grg <satyamgarg000@gmail.com>
2020-10-02 03:08:55 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,15 @@
 import 'package:flutter/material.dart';
 import 'screens/input_page.dart';
 
-void main() => runApp(BMICalculator());
+void main() => runApp(BMICalculator());   //App runs from here
 
 class BMICalculator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData.dark().copyWith(
-        primaryColor: Color(0xFF0A0E21),
-        scaffoldBackgroundColor: Color(0xFF0A0E21),
+      theme: ThemeData.dark().copyWith(     // choosing dark theme
+        primaryColor: Color(0xFF0A0E21),   //primary color - navi bluish color 
+        scaffoldBackgroundColor: Color(0xFF0A0E21), //same color for background of scaffold 
       ),
       home: InputPage(),
     );
