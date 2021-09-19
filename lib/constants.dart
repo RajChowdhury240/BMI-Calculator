42995e84210995d3b22aa8fc205d59f413f8bb75
Raj Chowdhury <30806882+Rajchowdhury420@users.noreply.github.com>
2021-09-19 12:18:22 +0530

    Update constants.dart

--- lib/constants.dart
+++ lib/constants.dart
@@ -4,7 +4,6 @@ const kBottomContainerHeight = 80.0;
 const kActiveCardColour = Color(0xFF1D1E33);
 const kInactiveCardColour = Color(0xFF111328);
 const kBottomContainerColour = Color(0xFFEB1555);
-
 const kLabelTextStyle = TextStyle(
   fontSize: 18.0,
   color: Color(0xFF8D8E98),
@@ -35,7 +34,6 @@ const kBMITextStyle = TextStyle(
   fontSize: 100.0,
   fontWeight: FontWeight.bold,
 );
-
 
 const kBodyTextStyle = TextStyle(
   fontSize: 22,
